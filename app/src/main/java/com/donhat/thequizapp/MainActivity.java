68b3c32bc7ed46af37b82b15c1f2d93942bf21f6
Do Nhat <dmnhat.pt@gmail.com>
2024-04-11 16:11:15 +0700

    Displaying all questions.

--- app/src/main/java/com/donhat/thequizapp/MainActivity.java
+++ app/src/main/java/com/donhat/thequizapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.donhat.thequizapp;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.donhat.thequizapp.databinding.ActivityMainBinding;
 import com.donhat.thequizapp.models.Question;
 import com.donhat.thequizapp.viewmodels.QuizViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private QuizViewModel _quizViewModel;
     private List<Question> _questionList;
 
-    static int result = 0;
-    static int totalQuestions = 0;
-    int i = 0;
+    private static int _result = 0;
+    private static int _totalQuestions = 0;
+    private int _i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +42,15 @@ public class MainActivity extends AppCompatActivity {
         _activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         // Resetting the Scores
-        result = 0;
-        totalQuestions = 0;
+        _result = 0;
+        _totalQuestions = 0;
 
         // Creating an instance of QuizViewModel
         _quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+
+        displayFirstQuestion();
+
+        _activityMainBinding.nextBtn.setOnClickListener(v -> displayNextQuestion());
     }
 
     private void displayFirstQuestion() {
@@ -59,5 +63,54 @@ public class MainActivity extends AppCompatActivity {
             _activityMainBinding.radioButton3.setText(questions.get(0).getOption3());
             _activityMainBinding.radioButton4.setText(questions.get(0).getOption4());
         });
+    }
+
+    private void displayNextQuestion() {
+        int selectedOption = _activityMainBinding.optionsRadioGroup.getCheckedRadioButtonId();
+        if (selectedOption != -1) {
+            RadioButton radioButton = findViewById(selectedOption);
+
+            // More questions to display
+            if (_questionList.size() - _i > 0) {
+                // Getting the number of questions
+                _totalQuestions = _questionList.size();
+
+                // Check if the chosen option is correct
+                if (radioButton.getText().toString().equals(
+                        _questionList.get(_i).getCorrectOption())
+                ) {
+                    _result++;
+                    _activityMainBinding.resultTextView.setText("Correct Answer: " + _result);
+                }
+
+                if (_i == 0) {
+                    _i++;
+                }
+
+                _activityMainBinding.questionTextView.setText("Question " + (_i + 1) + " : " + _questionList.get(_i).getQuestion());
+                _activityMainBinding.radioButton1.setText(_questionList.get(_i).getOption1());
+                _activityMainBinding.radioButton2.setText(_questionList.get(_i).getOption2());
+                _activityMainBinding.radioButton3.setText(_questionList.get(_i).getOption3());
+                _activityMainBinding.radioButton4.setText(_questionList.get(_i).getOption4());
+
+                // Check if it is the last question
+                if (_i == _questionList.size() - 1) {
+                    _activityMainBinding.nextBtn.setText("Finish");
+                }
+
+                _activityMainBinding.optionsRadioGroup.clearCheck();
+                _i++;
+            } else if (radioButton.getText().toString().equals(
+                    _questionList.get(_i - 1).getCorrectOption()
+            )) {
+                _activityMainBinding.resultTextView.setText("Correct Answer: " + _result);
+            }
+        } else {
+            Toast.makeText(
+                    this,
+                    "You need to make a selection!",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
     }
 }
--- app/src/main/java/com/donhat/thequizapp/retrofit/RetrofitInstance.java
+++ app/src/main/java/com/donhat/thequizapp/retrofit/RetrofitInstance.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
-    private String _baseUrl = "http://127.0.0.1/quiz/";
+    private String _baseUrl = "http://192.168.1.11/quiz/";
 
     public Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
